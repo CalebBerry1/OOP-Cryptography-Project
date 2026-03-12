c46d21d53a5e20a8ea9cd02ac44baa43c5d90322
Braeden/Bacon <braeden.lamb.bacon@gmail.com>
2026-03-12 15:34:51 -0400

    CREATED a branch for the XOR cipher. Currently the XOR cipher is operational, we just have to adjust some things depending on how we want to take output from it to use elsewhere. Commit also contains some changes to other files such as Presets and Settings, which may impact success on other devices.

---
+++ testFolder/testFile.cpp
@@ -0,0 +1,12 @@
+// test file
+
+// the XOR cipher takes an input target string, generates a random key, then uses the key to encrypt the target
+#include <iostream>
+
+std::string target;
+std::string key;
+std::string result;
+
+int main() {
+    std::cout << "Hello Pyrrhia and BAB and BOOBLES and more testing";
+}
