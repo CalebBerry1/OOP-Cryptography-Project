46164dfed2bb11f5c1a04070ebee473d29986ecf
CalebBerry1 <berry.caleb@hotmail.com>
2026-03-20 23:40:42 -0400

    Made and wired up EncryptPage

--- src/Components/app/mainWindow.cpp
+++ src/Components/app/mainWindow.cpp
@@ -6,6 +6,7 @@
 #include <QKeySequence>
 
 #include "../pages/homePage.h"
+#include "../pages/encryptPage.h"
 #include "AppStyles.h"
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
@@ -18,8 +19,17 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
     setCentralWidget(stack_);
 
     home_ = new HomePage(stack_);
+    encrypt_ = new EncryptPage(stack_);
 
     stack_ -> addWidget(home_);
+    stack_ -> addWidget(encrypt_);
+
+    connect(home_, &HomePage::encryptRequested, this, [this]() {
+        stack_ -> setCurrentWidget(encrypt_);
+    });
+    connect(encrypt_, &EncryptPage::backRequested, this, [this]() {
+        stack_ -> setCurrentWidget(home_);
+    });
 
     
     setupShortcuts();
