8db92f0c2cf1e5be0f5e784b110f07aa78219f4d
Braeden/Bacon <braeden.lamb.bacon@gmail.com>
2026-03-30 13:43:20 -0400

    Changes to XorStreamCipher and FeistelCipher

---
+++ src/algorithms/FeistelCipher.cpp
@@ -0,0 +1,44 @@
+// the Feistel cipher
+#include <iostream>
+#include <bitset>
+#include <string>
+#include "helper/makeStringToBinaryString.h"
+
+
+// round function example: convert data and key to ASCII, increase ASCII values by key
+std::string roundFunction(std::string data, int key) {
+
+    // iterates through each character in the data input, increasing its ASCII value by the key
+    for (char c : data) {
+        ascii = c;
+        ascii += key;
+
+        // converts the new character from int to binary
+        output += makeStringToBinary(std::to_string(ascii));
+    }
+
+    return output;
+}
+
+// function to split the data input into two halves, assigning the halves to "leftHalf" and "rightHalf"
+void splitData(std::string data) {
+
+    // collects the first half of data to "leftHalf"
+    while (index < (data.length()/2) ) {
+        leftHalf += data[index];
+        index++;
+    }
+
+    // collects the second half of data to "rightHalf"
+    while (index < (data.length())) {
+        rightHalf += data[index];
+        index++;
+    }
+}
+
+int main() {
+    std::cout << "Enter your message to encrpyt: \n";
+    std::cin >> input;
+    
+    return 0;
+}
--- src/algorithms/FeistelCipher.h
+++ src/algorithms/FeistelCipher.h
@@ -1,10 +1,19 @@
 #pragma once
 
+#include <bitset>
+#include <string>
+#include <vector>
 #include "../core/ICipher.h"
 
 class FeistelCipher : public ICipher {
     public:
-        CryptoResult run(const CryptoRequest& rec, ICryptoStepSink& sink) override {
-            return CryptoResult{false, {}, "FeistelCipher is not yet implemented"};
-        }
+        CryptoResult run(const CryptoRequest& req, ICryptoStepSink& sink) override;
+    
+    private:  
+    std::string input;
+    std::string output;
+    std::string leftHalf;
+    std::string rightHalf;
+    int index = 0;
+    int ascii;
 };
